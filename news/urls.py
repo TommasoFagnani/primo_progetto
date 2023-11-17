5c373c3028578d85a9731aded47791f9ad4522a9
TommasoFagnani <tommaso.fagnani@alessandrinimainardi.edu.it>
2023-11-17 09:50:06 +0100

    passo 18/19

--- news/urls.py
+++ news/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import home,queryBase, articoloDetailView, lista_articoli_giornalista
+from .views import home,indexNews,queryBase, articoloDetailView, lista_articoli_giornalista
 
 app_name = 'news'
 
 urlpatterns = [
     path('', home, name="homeview"),
+    path("indexNews",indexNews, name="indexNews"),
     path("articoli/<int:pk>",articoloDetailView, name="articolo_detail"),
     path("lista_articoli/<int:pk>",lista_articoli_giornalista, name="lista_articoli"),
     path("lista_articoli",lista_articoli_giornalista, name="lista_articoli"),
